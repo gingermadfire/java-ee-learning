8377b17e0a75401625d0976d9799502124edfd7a
Maxim Khusnutdinov <Maxim.ilian98@yandex.ru>
2023-05-03 00:53:59 +0300

    Test how works jsp

--- src/main/java/com/gingermadfire/javaeelearning/HelloServlet.java
+++ src/main/java/com/gingermadfire/javaeelearning/HelloServlet.java
@@ -2,17 +2,24 @@ package com.gingermadfire.javaeelearning;
 
 import java.io.*;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 public class HelloServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
 
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>Hello Servlet!</h1>");
+        out.println("<h1>Hello, " + name+ " " + surname +"</h1>");
         out.println("</body></html>");
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/redirect-servlet");
+        dispatcher.forward(request, response);
     }
 }
---
+++ src/main/java/com/gingermadfire/javaeelearning/test/TestClass.java
@@ -0,0 +1,7 @@
+package com.gingermadfire.javaeelearning.test;
+
+public class TestClass {
+    public String getInfo() {
+        return "This line is returning from test class";
+    }
+}
