c2fe8f3a9f6ca1f246adf8f7d4acea4e2890be34
Maxim Khusnutdinov <Maxim.ilian98@yandex.ru>
2023-05-03 15:20:01 +0300

    Testing sessions

---
+++ src/main/java/com/gingermadfire/javaeelearning/test/Cart.java
@@ -0,0 +1,31 @@
+package com.gingermadfire.javaeelearning.test;
+
+public class Cart {
+
+    private String name;
+
+    private int quantity;
+
+    public Cart() {}
+
+    public Cart(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
